d8293229743371b570608342792b3e90dded8578
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-06-08 15:16:55 +0300

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-import sys
+if __name__ == '__main__':
+    import sys
 
-args = sys.argv
-argc = len(args)
+    args = sys.argv
+    argc = len(args)
 
-if argc == 1:
-    print('0 arguments .')
-else:
-    print(f"{argc - 1} arguments :")
-    for i, c in enumerate(args):
-        if i == 0:
-            continue
-        print(f"{i:d}: {c}")
+    if argc == 1:
+        print('0 arguments .')
+    else:
+        print(f"{argc - 1} arguments :")
+        for i, c in enumerate(args):
+            if i == 0:
+                continue
+            print(f"{i:d}: {c}")
