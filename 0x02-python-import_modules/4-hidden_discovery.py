d1f42237d76b675bf68b09f09cc10ff6fc42a1a7
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-06-08 18:57:15 +0300

    task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,7 @@
 if __name__ == '__main__':
     import hidden_4
 
-    names = dir(hidden_4).sort()
+    names = dir(hidden_4)
     for c in names:
         if c startwith('__'):
             continue
