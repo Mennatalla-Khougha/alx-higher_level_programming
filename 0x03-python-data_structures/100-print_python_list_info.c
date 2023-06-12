da6e8ddb84f9efe7c5d09efd4c3daaa3cf8bfc19
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-06-12 15:03:04 +0300

    task 14

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -7,16 +7,16 @@
 
 void print_python_list_info(PyObject *p)
 {
-	int size, i;
+	Py_ssize_t size, i;
 	PyObject *list;
 
-	size = Py_SIZE(p);
-	printf("[*] Size of the Python List = %d\n", size);
-	printf("[*] Allocated = %d\n", ((PyListObject *)p)->allocated);
+	size = PyList_Size(p);
+	printf("[*] Size of the Python List = %zd\n", size);
+	printf("[*] Allocated = %zd\n", ((PyListObject *)p)->allocated);
 
 	for (i = 0; i < size; i++)
 	{
 		list = PyList_GetItem(p, i);
-		printf("Element %d: %s\n", i, Py_TYPE(list)->tp_name);
+		printf("Element %zd: %s\n", i, Py_TYPE(list)->tp_name);
 	}
 }
