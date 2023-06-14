f0902f01d02472e0493b8d501f4461ca420b1f72
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-06-14 18:09:54 +0300

    task 15

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    new_dict = {}
+    for key, val in a_dictionary.items():
+        if val == value:
+            continue
+        new_dict[key] = val
+    return new_dict
