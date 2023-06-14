457372afbc7ca9c789d950804ff162daca5ae35f
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-06-14 16:04:27 +0300

    task 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    sum = 0
+    if not roman_number:
+        return num
+
+    for c in roman_number:
+        if c == 'I':
+            sum += 1
+        elif c == 'V':
+            sum += 5
+        elif c == 'X':
+            sum += 10
+        elif c == 'L':
+            sum += 50
+        elif c == 'C':
+            sum += 100
+        elif c == 'D':
+            sum += 500
+        elif c == 'M':
+            sum += 1000
+    return sum
