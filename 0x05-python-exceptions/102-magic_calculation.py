306cc8a3e4ec9f2d7630687ead2a20a6c5d17d62
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-06-26 23:45:12 +0300

    task 9

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    result = 0
+
+    for i in range(1, 4):
+        try:
+            if a > i:
+                raise Exception("Too far")
+            result += (a ** b) / i
+        except Exception:
+            result = a + b
+            break
+    return result
