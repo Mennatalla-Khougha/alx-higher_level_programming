772265583b6ba6d02a24fd2ef5534b1169f37ba4
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-06-26 14:41:50 +0300

    task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    index = 0
+    num = 0
+    while index < x:
+        try:
+            print("{:d}".format(my_list[index]), end='')
+            index += 1
+            num += 1
+        except (ValueError, TypeError):
+            index += 1
+    print('')
+    return num
