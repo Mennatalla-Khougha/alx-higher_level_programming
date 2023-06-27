9b11706112169596cb41c091a71a96858957c80c
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-06-27 15:59:03 +0300

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-"""
-Define a class square
-"""
+"""Define a class square"""
 
 
 class Square:
-    """
-    A class square the defines a square
-    """
+    """A class square the defines a square"""
     def __init__(self, size):
+        """initialize a new square
+
+        Args:
+            size (int): The size of the square
+        """
         self._size = size
