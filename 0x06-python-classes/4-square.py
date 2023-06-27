d41db6bd8f1d4edf347383fd3ac71d1727fc7fd1
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-06-27 17:01:15 +0300

    task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Define a class square.
+"""
+
+
+class Square:
+    """
+    A class square the defines a square.
+    """
+
+    def __init__(self, size=0):
+        """
+        initialize a new Square.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Access the size property"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Return the current square area"""
+        return (self.__size * self.__size)
