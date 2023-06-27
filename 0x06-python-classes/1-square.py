4375a41bc3167ed7a305a32aceeacb1f80671f8c
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-06-27 16:10:30 +0300

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-"""Define a class square."""
+"""
+Define a class square.
+"""
 
 
 class Square:
-    """A class square the defines a square."""
+    """
+    A class square the defines a square.
+    """
 
     def __init__(self, size):
-        """initialize a new Square.
+        """
+        initialize a new Square.
 
         Args:
             size (int): The size of the square.
