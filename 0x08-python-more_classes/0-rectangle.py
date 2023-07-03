9d8cee2e2ef0f1ea34216c9c96c190118ed5e53c
Mennatalla <menmen.khougha@gmail.com>
2023-07-03 05:28:54 -0700

    task 0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This Module contains the definition of a rectangle
+"""
+
+
+class Rectangle:
+    """
+    This class represents a rectangle
+    """
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+We learn more about Pyton classes and objects. We learn by solving 11 tasks 10 mandatory tasks and 1 advanced task
