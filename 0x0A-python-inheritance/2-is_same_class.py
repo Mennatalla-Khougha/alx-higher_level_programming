9e7acb6b141f5267b4361e4f4fee13e338bd8c5d
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-07-10 15:14:26 +0300

    task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This modula check the class of an object"""
+
+
+def is_same_class(obj, a_class):
+    """Define a way to check the class of an abject
+
+    Args:
+        obj (object): object to be checked
+        a_class (class): class to be checked against
+
+    Returns:
+        bool: True or False
+    """
+    if type(obj) == a_class:
+        return True
