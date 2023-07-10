d71c55aff9462aec84a3459bfa2bfc661f46af76
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-07-10 16:44:11 +0300

    task 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""This modula contain a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """A class baseGeometry
+    """
+    def __init__(self):
+        pass
+
+    def area(self):
+        """Not implemented"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Function to validate the value of a name
+
+        Args:
+            name (str): name string
+            value (int): integer value greater than 0
+
+        Raises:
+            TypeError: when value not int
+            ValueError: when value <= 0
+        """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
