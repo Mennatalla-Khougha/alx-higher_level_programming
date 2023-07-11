7f1e65c262b0cc44f4eeef2505ec5c3087c9b841
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-07-12 02:17:50 +0300

    task 14

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -8,27 +8,33 @@ status_count = {}
 codes = [200, 301, 400, 401, 403, 404, 405, 500]
 line_count = 0
 
-for line in sys.stdin:
-    if line_count % 10 == 0 or KeyboardInterrupt:
+try:
+    for line in sys.stdin:
+        if line_count % 10 == 0:
+            print("File size: {}".format(file_size))
+            for code in sorted(status_count.keys()):
+                if status_count[code] > 0:
+                    print("{}: {}".format(code, status_count[code]))
+        line_count += 1
+        parts = line.split()
+        try:
+            file_size += int(parts[-1])
+        except (ValueError, IndexError):
+            pass
+        try:
+            status_code = int(parts[-2])
+            if status_code in codes:
+                if status_count.get(status_code, -1) == -1:
+                    status_count[status_code] = 1
+                else:
+                    status_count[status_code] += 1
+        except IndexError:
+            pass
         print("File size: {}".format(file_size))
         for code in sorted(status_count.keys()):
             if status_count[code] > 0:
                 print("{}: {}".format(code, status_count[code]))
-    line_count += 1
-    parts = line.split()
-    try:
-        file_size += int(parts[-1])
-    except (ValueError, IndexError):
-        pass
-    try:
-        status_code = int(parts[-2])
-        if status_code in codes:
-            if status_count.get(status_code, -1) == -1:
-                status_count[status_code] = 1
-            else:
-                status_count[status_code] += 1
-    except IndexError:
-        pass
+except KeyboardInterrupt:
     print("File size: {}".format(file_size))
     for code in sorted(status_count.keys()):
         if status_count[code] > 0:
