0fd42e73fa9a209469f042133666fea24a226863
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-07-11 22:07:10 +0300

    task 9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""This module write a class"""
+
+
+class Student:
+    """Define a Student class
+    """
+    def __init__(self, first_name, last_name, age):
+        """Initialize Student class
+
+        Args:
+            first_name (str): first name
+            last_name (str): last name
+            age (int): age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """retrieve a dictionary representation
+
+        Returns:
+            dict: dictionary
+        """
+        return self.__dict__
