1ef6cf157547e2970bec06563b9e5fe84fe19f40
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-08-15 18:16:26 +0300

    task 17

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,2 +1,2 @@
 -- converts hbtn_0c_0 database to UTF8 (utf8mb4)
-ALTER DATABASE `hbtn_0c_0` CHARACTER SET = utf8 COLLATE=utf8_unicode_ci;
+ALTER DATABASE `hbtn_0c_0` CHARACTER SET = utf8mb4 COLLATE=utf8_unicode_ci;
