8f86c44b74180121e52d5b130794889500996eb3
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-08-16 15:33:11 +0300

    task 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- lists all cities contained in the database hbtn_0d_usa.
 SELECT `id`, `name`, `name` 
-FROM `cities` JOIN BY `state` ON `id`
+FROM `cities` JOIN BY `state` ON `cities`.`state_id` = `state`.`id`
 ORDER BY `id`;
