0a4590385f7819d883f449b50156e2d784626a3e
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-08-17 00:26:50 +0300

    task 16

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,8 +1,8 @@
 --  lists all shows, and all genres linked to that show
-SELECT `tv`.`title` , `gen`.`name`
-FROM `tv_shows` AS `tv`
-LEFT JOIN `tv_show_genres` as `tv_s_g` 
-ON `tv_s_g`.`show_id` = `tv`.`id`
-LEFT JOIN `tv_genres` AS `gen`
-ON `tv_s_g`.`genre_id` = `gen`.`id`
-ORDER BY `tv`.`title`, `gen`.`name`;
+SELECT tv_shows.title, tv_genres.name 
+FROM tv_shows 
+LEFT JOIN tv_show_genres 
+ON tv_show_genres.show_id = tv_shows.id 
+LEFT JOIN tv_genres 
+ON tv_show_genres.genre_id = tv_genres.id 
+ORDER BY tv_shows.title, tv_genres.name ;
