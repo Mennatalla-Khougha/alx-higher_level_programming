573b91d0f29912a16e66a71410d621752f8d4a1d
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-08-16 16:21:20 +0300

    task 12

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,7 @@
+-- lists all shows contained in hbtn_0d_tvshows without a genre linked
+SELECT `tv`.`title`, `gen`.`genre_id`
+FROM `tv_shows` AS `tv`
+LEFT JOIN `tv_show_genres` AS `gen`
+ON `tv`.`id` = `gen`.`genre_id`
+WHERE `gen`.`genre_id` IS NULL
+ORDER BY `tv`.`title`;
