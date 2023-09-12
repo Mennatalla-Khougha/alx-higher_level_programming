53ee4f2701504793225fbef6ddd298cfe7cac1e7
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-09-12 12:55:22 +0300

    task 13

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,12 +1,17 @@
 #!/usr/bin/node
 const fs = require('fs');
+const { argv } = require('process')
 
-fs.readFile('fileA', (err, date) => {
+const first = argv[2]
+const second = argv[3]
+const into = argv[4]
+
+fs.readFile(first, (err, date) => {
   if (err) throw err;
-  fs.readFile('fileB', (err, date2) => {
+  fs.readFile(second, (err, date2) => {
     if (err) throw err;
     const str = date + date2;
-    fs.writeFile('fileC', str, (err) => {
+    fs.writeFile(into, str, (err) => {
       if (err) throw err;
     });
   });
