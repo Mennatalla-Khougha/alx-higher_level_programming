e4f19a3b6ef63c0b03cf858444280b8d7dc06bf3
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-09-12 12:41:30 +0300

    task 1

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
-module.exports = class Rectangle{
-  width;
-  height;
-
+module.exports = class Rectangle {
   constructor (w, h) {
     this.width = w;
     this.height = h;
   }
-}
-
+};
