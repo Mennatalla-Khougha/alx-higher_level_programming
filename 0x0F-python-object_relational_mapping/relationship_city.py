f17cd6b80441ff88d7d6a9c9bf328ad0aa577953
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-09-16 00:11:57 +0300

    task 15

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""python file contains the class definition of a city """
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+
+
+Base = declarative_base()
+
+
+class City(Base):
+    """class definition of a City
+
+    Args:
+        Base (class): declarative base
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"))
