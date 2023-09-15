82753ffd3b40c4959ba107de22e81e66542ce38f
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-09-15 15:25:41 +0300

    tasks 8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -11,7 +11,9 @@ if __name__ == '__main__':
         @localhost/{argv[3]}', pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    # rows = session.query(State)
-    for row in session.query(State).filter_by(id=1):
+    row = session.query(State).first()
+    if row:
         print(f'{row.id}: {row.name}')
+    else:
+        print('Nothing')
     session.close()
