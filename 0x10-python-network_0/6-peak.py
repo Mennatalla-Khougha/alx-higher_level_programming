42ead1f6b108502bc7fd174cd8d77b8fc2c1da7e
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-09-28 13:10:17 +0300

    task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""function that finds a peak in a list of unsorted integers"""
+
+
+def find_peak(list_of_integers):
+    """finds a peak in a list of unsorted integers"""
+    if not list_of_integers:
+        return None
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
+    if len(list_of_integers) == 2:
+        return max(list_of_integers)
+    left, right = 0, len(list_of_integers) - 1
+    mid = (left + right) // 2
+    peak = list_of_integers[mid]
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+        return peak
+    elif peak < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
+    else:
+        return find_peak(list_of_integers[mid + 1:])       
