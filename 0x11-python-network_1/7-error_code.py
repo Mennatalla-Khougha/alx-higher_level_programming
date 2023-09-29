a1975d23b9b9e1c884e553d08dbe4b21f43e3223
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-09-29 18:13:16 +0300

    task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+""" script that takes in a URL, sends a request to the URL"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    try:
+        html = requests.get(argv[1])
+        html.raise_for_status()
+        print(html.text)
+    except requests.RequestException as e:
+        print(f'Error code: {html.status_code}')
