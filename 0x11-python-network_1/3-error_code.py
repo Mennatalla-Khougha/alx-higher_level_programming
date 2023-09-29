1772537fa4868f0e567fe75224c073e08fddad09
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-09-29 16:44:35 +0300

    task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+""" script that takes in a URL, sends a request to the URL"""
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from sys import argv
+
+
+if __name__ == "__main__":
+    try:
+        with urlopen(argv[1]) as response:
+            print(response.read().decode('utf-8'))
+    except HTTPError as e:
+        print(f'Error code: {e.code}')
