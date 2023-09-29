f786d4ae7e4555bf9603ccce81b195941ed4efee
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-09-29 16:50:17 +0300

    task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""script that fetches https://alx-intranet.hbtn.io/status"""
+import requests
+
+
+if __name__ == "__main__":
+    html = requests.get('https://alx-intranet.hbtn.io/statu')
+    print("Body response:")
+    print(f'\t- type: {type(html)}')
+    print(f'\t- content: {html}')
