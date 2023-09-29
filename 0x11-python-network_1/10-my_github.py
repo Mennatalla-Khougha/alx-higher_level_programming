484b19e371df427bbb062b15cdc59890b3c17ac3
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-09-29 18:34:06 +0300

    task 9

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""script that takes your GitHub credentials"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    git_api = 'https://api.github.com/user'
+    data = (argv[1], argv[2])
+    user = requests.get(git_api, auth=data).json()
+    print(user.get("id"))
