dc5478e0ffe6a1b1250cc10e9daa22f0d3b79c86
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-09-29 16:17:12 +0300

    task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+""" script that takes in a URL, sends a request to the URL"""
+from urllib.request import urlopen
+from sys import argv
+
+
+with urlopen(argv[1]) as response:
+    print(response.getheader('X-Request-Id'))
