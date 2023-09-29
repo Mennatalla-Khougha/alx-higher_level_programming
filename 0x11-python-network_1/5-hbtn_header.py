e1a057b590590d39bd02df5521abd17172898b69
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-09-29 17:00:35 +0300

    task 5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+""" script that takes in a URL, sends a request to the URL"""
 import requests
 from sys import argv
 
 if __name__ == "__main__":
-    print(requests.get(argv[1]).getheader('X-Request-Id'))
+    html = requests.get(argv[1])
+    print(html.headers.get('X-Request-Id'))
