1852d22bbf9788416cda0823b70854170bb724ff
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-09-29 16:55:04 +0300

    task 3

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,7 +4,7 @@ import requests
 
 
 if __name__ == "__main__":
-    html = requests.get('https://alx-intranet.hbtn.io/statu')
+    html = requests.get('https://alx-intranet.hbtn.io/status').text
     print("Body response:")
     print(f'\t- type: {type(html)}')
     print(f'\t- content: {html}')
--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    print(requests.get(argv[1]).getheader('X-Request-Id'))
