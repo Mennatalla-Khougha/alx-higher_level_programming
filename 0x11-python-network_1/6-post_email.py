2769a9c69d708c04ce68352da4851fc362522162
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-09-29 17:04:09 +0300

    task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+""" script that takes in a URL and an email, sends a POST request """
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    mail = {"email": argv[2]}
+    html = requests.post(argv[1], mail)
+    print(html.text)
