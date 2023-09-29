58c077ba5bd1cf2d009ee9e5c93790fe52edc9ce
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-09-29 16:33:54 +0300

    task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+""" script that takes in a URL and an email, sends a POST request """
+from urllib.request import urlopen
+from urllib.parse import urlencode
+from sys import argv
+
+if __name__ == "__main__":
+    mail = {"email": argv[2]}
+    mail = urlencode(mail).encode('utf-8')
+    with urlopen(argv[1], data=mail) as response:
+        print(response.read().decode('utf-8'))
