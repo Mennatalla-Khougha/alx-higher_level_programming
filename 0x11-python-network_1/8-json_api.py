4d814b84ab74866a3828561e99211abe701d4e56
Mennatalla-Khougha <menmen.khougha@gmail.com>
2023-09-29 18:23:37 +0300

    task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+""" script that takes in a letter and sends a POST request"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    letter = {"q": argv[1] if len(argv) > 1 else ""}
+    html = requests.post('http://0.0.0.0:5000/search_user', letter)
+    try:
+        response = html.json()
+        if response:
+            print(f'[{response.get("id")}] {response.get("name")}')
+        else:
+            print('No result')
+    except ValueError:
+        print('Not a valid JSON')
